66ea0dfc86aca4d3b40cf80e7195c73d8d105d5f
Vishnu Pillai <135986853+VishnuP02@users.noreply.github.com>
2024-05-20 21:33:49 -0400

    Final Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Backend Developer", "Data Analyst", "Data Warehouse Developer", "Frontend Developer", "IT Support Specialist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+})
